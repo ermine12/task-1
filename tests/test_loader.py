1882e9a75553b8c4258d47d66825798327344b52
ermine12 <erminemfs@gmail.com>
2025-11-25 20:50:58 +0300

    correcting some stuff

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -6,7 +6,8 @@ import pytest
 # Add the src directory to the path so we can import modules
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-from descriptive import get_headline_stats
+from scripts.descriptive import get_headline_stats
+
 
 def test_headline_stats():
     """
